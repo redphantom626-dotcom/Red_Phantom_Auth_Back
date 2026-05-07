184dd165aecfc342d5b3a850fc7eb2f9e9f2ad16
redphantom626-dotcom <redphantom626@gmail.com>
2026-05-07 16:45:25 +0300

    Update auth.middleware.js

--- src/Middleware/auth.middleware.js
+++ src/Middleware/auth.middleware.js
@@ -1,10 +1,20 @@
 import jwt from "jsonwebtoken";
 
 export const auth = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
-  if (!token) return res.status(401).json({ message: "Unauthorized" });
+  const token = req.headers.token;
 
-  const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  req.user = decoded;
-  next();
+  if (!token || token === "undefined") {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: "Invalid token",
+      error: error.message,
+    });
+  }
 };
