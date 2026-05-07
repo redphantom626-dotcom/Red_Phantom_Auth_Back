091012ea1178d2ad3818142ab8f24114cb4a5dc9
redphantom626-dotcom <redphantom626@gmail.com>
2026-05-07 20:28:32 +0300

    Update auth.middleware.js

--- src/Middleware/auth.middleware.js
+++ src/Middleware/auth.middleware.js
@@ -1,35 +1,21 @@
 import jwt from "jsonwebtoken";
-import UserModel from "../DB/models/User.model.js";
 
 export const authentication = () => {
   return async (req, res, next) => {
     try {
-      const authHeader = req.headers.authorization;
+      const token = req.cookies.token;
 
-      if (!authHeader) {
-        return res.json({
-          message: "Token Required",
-        });
+      if (!token) {
+        return res.json({ message: "Token Required" });
       }
-
-      const token = authHeader.split(" ")[1];
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      const user = await UserModel.findById(decoded.id);
+      req.user = decoded;
 
-      if (!user) {
-        return res.json({
-          message: "User Not Found",
-        });
-      }
-
-      req.user = user;
       next();
     } catch (error) {
-      return res.json({
-        message: error.message,
-      });
+      return res.json({ message: error.message });
     }
   };
 };
